cc65e572c2fb31c01d0e8fa2c3a479b257db9222
Linxiao Chen <linxiao.chen0@gmail.com>
2013-08-25 12:42:41 +0200

    DOC: interpolate docstring change in interp2d

--- scipy/interpolate/interpolate.py
+++ scipy/interpolate/interpolate.py
@@ -146,17 +146,17 @@ class interp2d(object):
 
     >>> from scipy import interpolate
     >>> x = np.arange(-5.01, 5.01, 0.25)
-    >>> y = np.arange(-5.01, 5.01, 0.25)
-    >>> xx, yy = np.meshgrid(x, y)
+    >>> y = np.arange(-6.01, 6.01, 0.25)
+    >>> xx, yy = np.meshgrid(x, y, indexing='ij')
     >>> z = np.sin(xx**2+yy**2)
-    >>> f = interpolate.interp2d(x, y, z, kind='cubic')
+    >>> f = interpolate.interp2d(x, y, z.T, kind='cubic')
 
     Now use the obtained interpolation function and plot the result:
 
     >>> xnew = np.arange(-5.01, 5.01, 1e-2)
-    >>> ynew = np.arange(-5.01, 5.01, 1e-2)
-    >>> znew = f(xnew, ynew)
-    >>> plt.plot(x, z[0, :], 'ro-', xnew, znew[0, :], 'b-')
+    >>> ynew = np.arange(-6.01, 6.01, 1e-2)
+    >>> znew = f(xnew, ynew).T
+    >>> plt.plot(x, z[:, 0], 'ro-', xnew, znew[:, 0], 'b-')
     >>> plt.show()
 
     """
